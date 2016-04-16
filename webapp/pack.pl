85fcfbb003ea42150eb75147106ee191cf2247a9
lucarin91 <lucarin91@gmail.com>
2016-04-16 20:28:15 +0200

    update MonitorWebApp release package

--- webapp/pack.pl
+++
@@ -1,8 +0,0 @@
-@files = <*>;
-
-foreach $file (@files){
-    if ($file ne __FILE__){
-	print "packing $file ...\n";
-    	`zip -r ../MonitorWebApp-v0.1-${file}.zip $file`;
-    }
-}
---
+++ webapp/zip-build.pl
@@ -0,0 +1,24 @@
+use warnings;
+use strict;
+
+my $NAME  = 'MonitorWebApp';
+my $PATH  = "build/$NAME";
+my $V     = 'v0.2';
+my @files = <"$PATH/*">;
+
+foreach my $file (@files) {
+
+    # if ($file ne __FILE__){
+    $file =~ s#.*/##;
+    my $name = "$NAME-$V-$file";
+    print "packing $name..\n";
+    `mv $PATH/$file $PATH/$name`;
+    `cd $PATH && zip -r $name.zip $name`;
+    `rm -rvf $PATH/$name`;
+
+    # }
+}
+
+`mv $PATH/*.zip ../build`;
+`rm -rvf ./build`;
+print "all done!\n";
